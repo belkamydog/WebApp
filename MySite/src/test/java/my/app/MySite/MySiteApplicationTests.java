44022c66dd856f8d1931864564656cd2d16d357e
belkamydog <120400903+belkamydog@users.noreply.github.com>
2024-01-18 08:58:54 +0300

    start project

---
+++ MySite/src/test/java/my/app/MySite/MySiteApplicationTests.java
@@ -0,0 +1,13 @@
+package my.app.MySite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
